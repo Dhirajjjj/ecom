1f8e53aa69cbdef68647827501b9d26e4a4315dd
Dhirajjjj <patildhiraj2306@gmail.com>
2024-05-15 00:41:59 +0530

    updated UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [{path:'login', component:LoginComponent},
-                        {path:"signup", component:SignupComponent},
-                        {path:'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule),canActivate:[authGuard] },
-                        {path:'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),canActivate:[authGuard]}
+                        {path:"signup", component:SignupComponent}, //canActivate:[authGuard]
+                        {path:'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+                        {path:'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)}
                       ];
 
 @NgModule({
